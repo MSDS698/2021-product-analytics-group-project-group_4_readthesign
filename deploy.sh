8f6b6cda044f345e47810752b9ab8f5b6903a808
trevor-santiago <tjsantiago@dons.usfca.edu>
2021-06-07 15:07:04 -0700

    add PEM_NAME environment variable to deploy, remove API_KEY from docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ECHO "Hello, World!"
-eb init group_4_readthesign-app --region us-west-2 --platform Docker
-eb create group_4_readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb init group-4-readthesign-app --region us-west-2 --platform Docker
+eb create group-4-readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --key $PEM_NAME
