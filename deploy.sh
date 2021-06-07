0b01f3be4accb179a02b0bf11843d63b25c58c04
Anni-Liu <aliu21@usfca.edu>
2021-06-07 00:29:31 -0700

    add starter deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
 ECHO "Hello, World!"
+eb init group_4_readthesign-app --region us-west-2 --platform Docker
+eb create group_4_readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
