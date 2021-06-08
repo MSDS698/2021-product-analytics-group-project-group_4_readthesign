befb95469c05ad8a6d65b9e0f27001f417febcad
wonseok <wonseok@wonseoks-MacBook-Pro.local.ucsf.edu>
2021-06-07 21:25:57 -0700

    frontend updated

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -12,14 +12,14 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'ReadTheSign'
 copyright = '2021, ReadTheSign Developers'
-author = 'Kyle Brooks, Trevor Santiago, Efrem Ghebreab, Binaya Tripathi, Dawn (Shuyan Li), Ye Tao, Wonseok Choi, Anni Liu'
+author = 'Kyle Brooks, Trevor Santiago, Efrem Ghebreab, Dawn (Shuyan Li), Ye Tao, Wonseok Choi, Anni Liu'
 
 # The full version, including alpha/beta/rc tags
 release = 'v0'
@@ -30,8 +30,7 @@ release = 'v0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
