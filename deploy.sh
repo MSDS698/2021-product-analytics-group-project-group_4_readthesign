95b351bb5dd54061f720081dee1da5cfd0ea2bad
trevor-santiago <tjsantiago@dons.usfca.edu>
2021-06-07 17:41:44 -0700

    move --key arg in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ECHO "Hello, World!"
-eb init group-4-readthesign-app --region us-west-2 --platform Docker
-eb create group-4-readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --key $PEM_NAME
+eb init group-4-readthesign-app --region us-west-2 --platform Docker --key $PEM_NAME
+eb create group-4-readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY 
