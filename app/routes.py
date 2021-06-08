f3ea5c028a3a0665744bba3d0259955d7f8da8a2
wonseok <wonseok@wonseoks-MacBook-Pro.local.ucsf.edu>
2021-06-07 22:34:52 -0700

    add doc string

--- app/routes.py
+++ app/routes.py
@@ -9,16 +9,17 @@ team_list = [ 'Kyle', 'Trevor', 'Efrem', 'Choi', 'Anni', 'Dawn', 'Ye Tao', 'Bina
 
 @app.route('/')
 def index():
+	"""deployment of home page html"""
 	return render_template('index.html')
 
 
 @app.route('/about')
 def about():
+	"""deployment of about page html"""
 	return render_template('about.html')
 
 
 @app.route('/team')
 def team():
+	"""deployment of team page html"""
 	return render_template('team.html', names=team_list)
-
-	
