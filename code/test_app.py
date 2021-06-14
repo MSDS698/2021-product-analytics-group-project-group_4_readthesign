05dded10f950cfb367cf4acf8edb3e43fb06422a
efaro2014 <efarkbg@gmail.com>
2021-06-14 01:10:07 -0700

    test.py

--- code/test_app.py
+++ code/test_app.py
@@ -6,18 +6,12 @@ def UserFromDB(username):
 	user = classes.User.query.filter_by(username=username).first()
 	return user
 
-# def test_db_presence():
-# 	db = SQLAlchemy()
-# 	engine = db.create_engine(Config.SQLALCHEMY_DATABASE_URI, {})
-# 	inspect = db.inspect(engine)
-# 	assert (inspect.has_Table("user"))
+def test_db_presence():
+	db = SQLAlchemy()
+	engine = db.create_engine(Config.SQLALCHEMY_DATABASE_URI, {})
+	inspect = db.inspect(engine)
+	assert (inspect.dialect.has_table(engine, "user"))
 
-# def test_api_call():
-#     """
-#     unit test for app.py
-#     """
-#     assert app is not None
 def test_User():
 
 	assert UserFromDB("Efrem").username == "Efrem"
-
