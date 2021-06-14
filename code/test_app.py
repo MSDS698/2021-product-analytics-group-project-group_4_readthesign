3483782d0f74b5bc89389fe9427ba9fa68ce7c3b
efaro2014 <efarkbg@gmail.com>
2021-06-14 00:52:56 -0700

    test.py

--- code/test_app.py
+++ code/test_app.py
@@ -1,7 +1,23 @@
 from app import *
+from config import Config
+from flask_sqlalchemy import SQLAlchemy 
 
-def test_api_call():
-    """
-    unit test for app.py
-    """
-    assert app is not None
+def UserFromDB(username):
+	user = classes.User.query.filter_by(username=username).first()
+	return user
+
+# def test_db_presence():
+# 	db = SQLAlchemy()
+# 	engine = db.create_engine(Config.SQLALCHEMY_DATABASE_URI, {})
+# 	inspect = db.inspect(engine)
+# 	assert (inspect.has_Table("user"))
+
+# def test_api_call():
+#     """
+#     unit test for app.py
+#     """
+#     assert app is not None
+def test_User():
+
+	assert UserFromDB("Efrem").username == "Efrem"
+
