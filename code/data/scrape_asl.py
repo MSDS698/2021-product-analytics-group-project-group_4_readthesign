c48a18254c9433ef68de36302e4ef334f7003d6f
Kyle Brooks <54421499+kyledbrooks@users.noreply.github.com>
2021-06-14 11:09:26 -0700

    Efficiency and json generation improvements
    Implemented WebDriver wait for efficiency and cleaned up json generation

--- code/data/scrape_asl.py
+++ code/data/scrape_asl.py
@@ -1,13 +1,16 @@
-import time
 import json
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 
 def get_words(link):
     """
     Function that gathers common first ASL words for initial data.
+    From: https://www.handspeak.com/word/most-used/
     """
 
     hundred_words_html = requests.get(link).text
@@ -17,6 +20,8 @@ def get_words(link):
     html_list = soup.find('ol', attrs={'class': 'col-list'}).find_all('li')
 
     word_list = []
+
+    # Clean up html and populate words into list
     for i in html_list:
         word_list.append(i.text.replace('\n', ' ').strip())
 
@@ -25,20 +30,22 @@ def get_words(link):
 
 def get_video_urls(driver, word, res):
     """
-    Get URLs for videos associated with a particular phrase/word.
+    Get URLs for videos associated with a particular phrase/word and populate to dictionary.
     """
+
     videos = driver.find_elements_by_tag_name('video')
-    # videos = driver.find_elements_by_tag_name('source')
 
     url_list = []
     for video in videos:
         html = video.get_attribute('innerHTML')
+
+        # Hacky way to parse off associated URLs
         _, _, temp = html.partition('"')
         url, _, _ = temp.partition('"')
 
-        url_list.append(f"{url}")
+        url_list.append(url)
 
-    res[f"{word}"] = url_list
+    res[word] = url_list
 
     return res
 
@@ -47,24 +54,34 @@ def search_ASL(driver, words):
     """
     Searches all specified words on ASL website
     """
-    res = {}
+    res = {}  # Dictionary to house words as associated video urls
     for word in words:
-        time.sleep(3)
-        search_field = driver.find_element_by_xpath('/html/body/div[3]/div/div/form/div/div/div/input')
-        search_field.send_keys(word)
-        driver.find_element_by_xpath('/html/body/div[3]/div/div/form/div/div/div/span/button/span').click()
-        time.sleep(3)
+        search_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div/form/div/div/div/input'))
+        )
+
+        search_field.send_keys(word)  # adds word to search field
+
+        button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div/form/div/div/div/span/button/span'))
+        )
+
+        button.click()  # click search button
+
         res = get_video_urls(driver, word, res)
+
+
         driver.back()
-        driver.find_element_by_xpath('/html/body/div[3]/div/div/form/div/div/div/input').clear()
 
-#         with open('test', 'w') as f:
-#             f.write(str(res).replace("'", '"'))
-
-    return str(res)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div/div/form/div/div/div/input'))
+        ).clear()
 
 
-def main():
+    return res
+
+
+if __name__ == '__main__':
     words = get_words('https://www.handspeak.com/word/most-used/')
 
     driver = webdriver.Chrome('/usr/local/bin/chromedriver')
@@ -72,13 +89,10 @@ def main():
 
     res = search_ASL(driver, words)
 
-    res = res.replace("'", '"')
+    res = json.dumps(res)
 
-    res = json.loads(res)
-
-    return res
+    # Write json string to file
+    with open ('videos.json', 'w') as f:
+        f.write(res)
 
     driver.quit()
-
-
-main()
