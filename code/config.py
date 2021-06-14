b5599dda01a6e864e1767ac196ed05b03d5a8d06
Anni-Liu <aliu21@usfca.edu>
2021-06-13 23:11:42 -0700

    flask with AWS RDS

--- code/config.py
+++ code/config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'asl.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'asl.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:readthesign@msds603-group-4-readthesign.caomeghbqkwn.us-west-2.rds.amazonaws.com/postgres' 
     SQLALCHEMY_TRACK_MODIFICATIONS = True # flask-login uses sessions which require a secret Key
 
 
