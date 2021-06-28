ede09a7f4b8021f065d66a902ce84bb41c70ca7f
trevor-santiago <tjsantiago@dons.usfca.edu>
2021-06-27 21:37:57 -0700

    add instance type, ignore .git in eb

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ECHO "Hello, World!"
-eb init group-4-readthesign-app --region us-west-2 --platform Docker --key $PEM_NAME
-eb create group-4-readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY 
+eb init group-4-readthesign-app --region us-west-2 --platform Docker --keyname $PEM_NAME
+eb create group-4-readthesign-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --instance-types p2.xlarge
