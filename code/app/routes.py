48b44dbef231d62f6173cf08a4c3c1a0a1d625e9
wonseok <wonseok@wonseoks-MacBook-Pro.local>
2021-06-27 19:04:57 -0700

    added code for removing saved file

--- code/app/routes.py
+++ code/app/routes.py
@@ -43,9 +43,9 @@ def upload():
         file_path = os.path.join(file_dir_path, filename)
         # Save file to file_path (instance/ + 'files’ + filename)
         f.save(file_path)
-        result = pred(file_path)
+        result = pred(file_path)  # run prediction on input data
+        os.remove(file_path)
         output = 'Our Prediction:'
-        # result = pred(file_path)  # run prediction on input data
         return render_template('upload.html', form=file, result=result, output=output)  # Redirect to / (/index) page.
     return render_template('upload.html', form=file, authenticated_user=current_user.is_authenticated)
 
